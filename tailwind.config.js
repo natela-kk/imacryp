d4ba9df49676aad2d4d0550b9713261569c9ab08
Koltsova Aleksandra <valvic90@gmail.com>
2023-09-24 10:18:32 +0300

    button component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-black": "#121212",
+        "custom-neutral": "#161619",
+      },
+    },
   },
   plugins: [],
-}
+};
